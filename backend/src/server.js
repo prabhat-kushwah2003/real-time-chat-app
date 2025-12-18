d42bcaabc46d7ac18addbf655484ec4f7d95128f
prabhat-kushwah2003 <prabhatkushwah2003@email.com>
2025-12-18 14:58:51 +0530

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
